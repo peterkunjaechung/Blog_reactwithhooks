db2cab9d73a1bf7f8bb73fe97974d601e9256fa5
contactpeterchung.com <contactpeterchung@gmail.com>
2019-06-19 13:44:11 -0600

    Final commit

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,40 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, } from "react";
 import axios from "axios";
-import { List, Header, Segment, } from "semantic-ui-react";
+import BlogForm from "./BlogForm";
+import { List, Header, Segment, Button, } from "semantic-ui-react";
 
-class Blogs extends React.Component {
-  state = { blogs: [], };
+const Blogs = (props) => {
+  const [blogs, setBlogs] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
-  componentDidMount() {
+  useEffect( () => {
     axios.get("/api/blogs")
-      .then( res => {
-        this.setState({ blogs: res.data, });
-      })
-  }
+      .then( res => setBlogs(res.data) )
+  }, []);
 
-  renderBlogs = () => {
-    const { blogs, } = this.state;
-    return blogs.map( blog => (
+  const renderBlogs = () => {
+    return blogs.map( blog =>
       <Segment key={blog.id}>
-        <List.Header as="h3">{blog.title}</List.Header>
+        <List.Header as="h3">{ blog.title }</List.Header>
         <List.Description>
           { blog.body }
         </List.Description>
       </Segment>
-    ))
-  }
+    );
+  };
 
-  render() {
-    return (
-      <>
-        <Header as="h1">My Blogs</Header>
-        <br />
-        <List>
-          { this.renderBlogs() }
-        </List>
-      </>
-    )
-  }
-}
+  return (
+    <>
+      <Header as="h1">My Blogs</Header>
+      <br />
+      { showForm && 
+        <BlogForm 
+          toggleForm={setShowForm} 
+          add={ blog => setBlogs([...blogs, blog]) } 
+        /> 
+      }
+      <Button onClick={ () => setShowForm(!showForm) }>
+        { showForm ? "Close Form" : "Show Form" }
+      </Button>
+      <List>
+        { renderBlogs() }
+      </List>
+    </>
+  );
+};
 
-export default Blogs;
+export default Blogs
